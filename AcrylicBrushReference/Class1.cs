76f9eb24956a8e34662a254b8822dd98fbc07b1b
李新阳 <lixinyangemil@outlook.com>
2018-08-28 11:02:01 +0800

    添加项目文件。

---
+++ AcrylicBrushReference/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Hosting;
+
+namespace AcrylicBrushReference
+{
+    /// <summary>
+    ///亚克力笔刷
+    /// </summary>
+    public static class AcrylicBrush
+    {
+        /// <summary>
+        ///初始化亚克力效果
+        ///需要在XAML的外层Grid中添加Span所有网格的内层Grid
+        /// </summary>
+        /// <param name="glassHost">内层Grid对象的名称</param>
+        public static void InitializeFrostedGlass(UIElement glassHost)
+        {
+            Visual hostVisual = ElementCompositionPreview.GetElementVisual(glassHost);
+            Compositor compositor = hostVisual.Compositor;
+            var backdropBrush = compositor.CreateHostBackdropBrush();
+            var glassVisual = compositor.CreateSpriteVisual();
+            glassVisual.Brush = backdropBrush;
+            ElementCompositionPreview.SetElementChildVisual(glassHost, glassVisual);
+            var bindSizeAnimation = compositor.CreateExpressionAnimation("hostVisual.Size");
+            bindSizeAnimation.SetReferenceParameter("hostVisual", hostVisual);
+            glassVisual.StartAnimation("Size", bindSizeAnimation);
+            //界面扩展到标题栏
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = false;
+            var titlebar = ApplicationView.GetForCurrentView().TitleBar;
+            titlebar.ButtonBackgroundColor = Colors.Transparent;
+        }
+    }
+}
